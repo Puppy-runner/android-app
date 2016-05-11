c495ea2ee639f4f474a236cfa77ffb5e86a4d6c0
saulgar <saul.alejandro.garcia.garcia@gmail.com>
2016-05-10 20:37:38 -0500

    Add tabs with material design.

--- app/src/main/java/com/garcia/saul/puppy_runner/MainActivity.java
+++ app/src/main/java/com/garcia/saul/puppy_runner/MainActivity.java
@@ -8,6 +8,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +34,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MainPageAdapter());
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 
     @Override
@@ -48,5 +68,91 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class MainPageAdapter extends PagerAdapter {
+
+        private LinearLayout page1;
+        private LinearLayout page2;
+        private ListView page3;
+        private LinearLayout page4;
+        private final int[] titles = {R.string.page1, R.string.page2, R.string.page3, R.string.page4};
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getString(titles[position]);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup collection, int position) {
+            View page;
+            switch (position) {
+                case 0:
+                    if (page1 == null) {
+                        page1 = (LinearLayout) LayoutInflater.from(MainActivity.this).inflate(R.layout
+                                .page_one, collection, false);
+                    }
+                    page = page1;
+                    break;
+                case 1:
+                    if (page2 == null) {
+                        page2 = (LinearLayout) LayoutInflater.from(MainActivity.this).inflate(R.layout
+                                .page_two, collection, false);
+                    }
+                    page = page2;
+                    break;
+                case 2:
+                    if (page3 == null) {
+                        page3 = (ListView) LayoutInflater.from(MainActivity.this).inflate(R.layout
+                                .page_three, collection, false);
+                        initListView();
+                    }
+                    page = page3;
+                    break;
+                default:
+                    if (page4 == null) {
+                        page4 = (LinearLayout) LayoutInflater.from(MainActivity.this).inflate(R.layout
+                                .page_four, collection, false);
+                    }
+                    page = page4;
+                    break;
+            }
+
+            collection.addView(page, 0);
+
+            return page;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup collection, int position, Object view) {
+            collection.removeView((View) view);
+        }
+
+        private void initListView() {
+            String[] items = new String[50];
+            for (int i = 0; i < 50; i++) {
+                items[i] = "Item " + i;
+            }
+            page3.setAdapter(new ArrayAdapter<String>(MainActivity.this, R.layout.textview, items));
+            page3.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Toast.makeText(MainActivity.this, (String) parent.getItemAtPosition(position), Toast
+                            .LENGTH_SHORT).show();
+                }
+            });
+
+        }
     }
 }
